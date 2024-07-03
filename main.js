32c10407f0dc3929b9c025030a68470e945d12aa
j.dsouza <j.dsouza@freetimecompany.nl>
2024-07-03 08:53:29 +0200

    hello

--- main.js
+++ main.js
@@ -1,17 +1,34 @@
 const React = this.React;
 const ReactDOM = this.ReactDOM;
 const _jsx = this.jsx;
+const _jsxs = this.jsxs;
+const _Fragment = this.Fragment;
+
+function Display({
+    count
+}) {
+    return /*#__PURE__*/_jsx("h2", {
+        children: count
+    });
+}
 
 function App() {
     const [count, setCount] = React.useState(0);
-    debugger;
     React.useEffect(() => {
         debugger;
     }, []);
-    return /*#__PURE__*/_jsx("h1", {
-        children: "HELLO NO JSX"
+    return /*#__PURE__*/_jsxs(_Fragment, {
+        children: [" ", /*#__PURE__*/_jsx("h1", {
+            onClick: () => {
+                setCount(c => c + 1);
+            },
+            children: "HELLO NO JSX"
+        }), " ", /*#__PURE__*/_jsx(Display, {
+            count: count
+        }), " "]
     });
 }
+
 ReactDOM.createRoot(document.getElementById('root')).render( /*#__PURE__*/_jsx(React.StrictMode, {
     children: /*#__PURE__*/_jsx(App, {})
 }));
